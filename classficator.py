6f6233f68eec84e4a6a04b9e627f28d7aacd94f4
MassimoZarantonello2 <m.zarantonello2@campus.unimib.it>
2023-06-20 15:55:02 +0200

    Commit finale pre refactoring

--- classficator.py
+++ classficator.py
@@ -15,10 +15,14 @@ def get_features(images,names):
         if len(image.shape) == 3:
             image = image[:,:,0]
             
+        area = get_area(image)
+        perimeter = get_perimeter(image)
+        rapp = area/perimeter**2
+        convex = get_convex_feature(image)
         major_axis_length, minor_axis_length = get_axis(image)
-        rapp = major_axis_length/minor_axis_length
+        rapp_2 = major_axis_length/minor_axis_length
         compactness = get_compactness(image)
-        feature = [compactness,rapp]
+        feature = [compactness,rapp_2]
         features.append(feature)    
         
     return features,labels
@@ -38,9 +42,9 @@ def get_test_features(read_path):
 if __name__ == '__main__':
     knn = KNeighborsClassifier(n_neighbors=5,weights='distance')
     
-    train_fetures,train_labels = get_train_features('./results/')
-
-    find_best_seed(knn,train_fetures,train_labels,True)
+    fetures,labels = get_train_features('./results/')
+    
+    find_best_seed(knn,fetures,labels,True)
     
     
     
@@ -56,3 +60,13 @@ if __name__ == '__main__':
     
     
     #5 neighbors, distance weights, 0.2 test size, 0.8 train size, seed 299396
+    
+    #5 neighbors, distance weights, 0.2 test size, 0.8 train size, seed 524748 area e tutte cose
+    
+    #5 neighbors, distance weights, 0.2 test size, 0.8 train size, seed 863767 area 95%
+    
+    #5 neighbors, distance weights, 0.2 test size, 0.8 train size, seed 819055 area e invarianti 95%
+    
+    #5 neighbors, distance weights, 0.2 test size, 0.8 train size, seed 819055 invarianti 88% e 2 features
+    
+    
